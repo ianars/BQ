7c27fa2ffd92232d3f73c40ab6e2ca0397a9b1f1
ianars <iana.rusu.it@gmail.com>
2017-09-17 20:17:30 +0200

    added tab menu for desktop, added font-awesome icons, modified css styles

--- js/main.js
+++ js/main.js
@@ -16,22 +16,21 @@ for (var i = 0; i < smallMenuLinkList.length; i++) {
 }
 
 
-function openSection(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
+$(document).ready(function(){
+  $('ul.tabs li a:first').addClass('active');
+  $('.sections article').hide();
+  $('.sections article:first').show();
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+$('ul.tabs li a').click(function(){
+  $('ul.tabs li a').removeClass('active');
+  $(this).addClass('active');
+  $('.sections article').hide();
+
+    var activeTab = $(this).attr('href') ;
+    $(activeTab).show();
+    return false;
+})
+});
 
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
