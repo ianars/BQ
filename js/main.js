24b89503a600664039732c5343d8a5173559a7dd
ianars <iana.rusu.it@gmail.com>
2017-09-18 05:36:40 +0200

    changed nav menu for mobile version, added surf, skateboard, longboard andsnowboard section

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
 'use strict';
 
-var bigMac = document.getElementById("btn-bigMac");
-var btnNavShow = document.getElementById("btn-navShow");
-
-bigMac.addEventListener("click", mobile);
-function mobile() {
-  btnNavShow.classList.toggle("listShow");
-  bigMac.classList.toggle("open");
-}
-
-var header = document.querySelector(".nav");
-var smallMenuLinkList = document.querySelectorAll(".nav-mobile li a");
-for (var i = 0; i < smallMenuLinkList.length; i++) {
-  smallMenuLinkList[i].addEventListener("click", mobile);
-}
-
-
 $(document).ready(function(){
   $('ul.tabs li a:first').addClass('active');
   $('.sections article').hide();
